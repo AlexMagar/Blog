ce7aacab5c3a9a9e9c2985893ad9005ce9652a9c
AlexMagar <lx.mgr07@gmail.com>
2022-08-27 21:43:50 +1000

    additional files

---
+++ app.js
@@ -0,0 +1,31 @@
+//jshint esversion:6
+
+//require packages
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+
+const homeStartingContent ="Something for now";
+const aboutContent = "Also something for now";
+const contactContent = "Also the somethig for now";
+
+//creating express constant
+const app = express();
+
+app.set("view engine", "ejs");
+
+//load the file contain in public folder
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+
+
+//listen to port 3000
+app.listen(3000, function(req, res){
+    console.log("server started on port 3000");
+});
